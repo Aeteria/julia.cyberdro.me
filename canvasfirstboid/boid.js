8fd748479627a949d631f2a3065336974d9017ec
Julia Reid <jcar.reid@gmail.com>
2018-02-16 17:38:35 -0800

    particle sim commit

---
+++ canvasfirstboid/boid.js
@@ -0,0 +1,102 @@
+'use strict'
+
+
+// Start Boid class  +++++++++++++++++++++++++++++++++++++++++++++++++++++
+class Boid {
+  //  Boid constructor
+  constructor(m, location) {
+    // declare instance variables for Boid
+    this.main = m;
+    this.loc = location;
+    this.vel = vector2d(Math.random(-1.0, 10)*5, Math.random(-1.0, 10)*5);
+    this.acc = vector2d(0, GRAV)
+    this.rep = vector2d(5,5 )
+    this.repMult = .1;
+
+    this.context = this.main.context;
+    //create all initial items
+    this.init();
+  }
+
+  init(){
+
+  }
+
+  run() { // update this
+    //console.log("Inside boid run");
+    this.update();
+    this.render();
+  }
+
+//   var posRelativeToMouse = {
+//   x: this.x - mousPosX,
+//   y: this.y - mousPosY
+// };
+//
+// var distance = Math.sqrt(
+//   posRelativeToMouse.x * posRelativeToMouse.x +
+//   posRelativeToMouse.y * posRelativeToMouse.y
+// );
+// var forceDirection = {
+//   x: posRelativeToMouse.x / distance,
+//   y: posRelativeToMouse.y / distance,
+// };
+// var maxDistance = 1000;
+// var force = (maxDistance - distance) / maxDistance;
+//
+// // if we went below zero, set it to zero.
+// if (force < 0) force = 0;
+
+
+  update() { // render or draw this to canvas
+    this.checkEdges();
+    if(this.main.gravity === false){
+    this.loc.x+= this.vel.x;
+    this.loc.y+= this.vel.y;
+    //particle.xVel += forceDirection.x * force * 0.03;
+  //  particle.yVel += forceDirection.y * force * 0.03;
+
+    }
+    else if(this.main.gravity === true){
+      this.vel.x+= this.acc.x;
+      this.vel.y+= this.acc.y;
+      this.loc.x+= this.vel.x;
+      this.loc.y+= this.vel.y;
+    }
+    if(Math.abs(this.loc.dist(r.loc))<100){
+    console.log(this.loc, r.loc)
+      this.vel = this.loc.sub(r.loc);
+      console.log(this.vel);
+
+
+    }
+    if(this.vel.length() > 4){
+      this.vel.normalize();
+      this.vel.scale(4)
+    }
+  }
+
+
+  render() { // render or draw this to canvas
+    //console.log("loc.x = " + this.loc.x);
+    this.context.fillStyle = 'red' ;
+    //this.context.fill();
+
+    this.context.beginPath();
+    this.context.arc(this.loc.x, this.loc.y, 3, 0, 2 * Math.PI, false);
+    this.context.fill();
+    this.context.stroke();
+
+  }
+
+  checkEdges(){
+
+  //  if(this.loc.x > 1000 ||this.loc.x < 10) this.vel.x *= -1;
+    if(this.loc.x > 1000 && this.vel.x > 0){this.vel.x *= -1}
+    if(this.loc.x < 10 && this.vel.x < 0){this.vel.x *= -1}
+    if(this.loc.y > 750 && this.vel.y > 0) this.vel.y *= -1;
+    if(this.loc.y < 10 && this.vel.y < 0) this.vel.y *= -1;
+
+  }
+
+}
---
+++ canvasfirstboid/main.js
@@ -0,0 +1,197 @@
+'use strict'
+
+// wait for the window to load and than call back setup()
+window.addEventListener('load', setup, false);
+
+var main;   // the global Main object
+const FRAME_RATE=30;
+const NUM_BOIDS = 1;
+const GRAV = 0.2;
+const MAX_VEL = 4;
+function setup() {
+  main = new Main();
+  window.setTimeout(draw, 100);    // wait 100ms for resources to load then start draw loop
+}
+
+function draw() {   // the animation loop
+  main.run();
+  window.setTimeout(draw, 1000/FRAME_RATE);
+     // come back here every interval
+}
+
+var cursorX;
+var cursorY;
+var r
+document.onmousemove = function(e){
+    cursorX = e.offsetX;
+    cursorY = e.offsetY;
+}
+// Start main class  +++++++++++++++++++++++++++++++++++++++++++++++++++++
+class Main {
+  //  Main constructor
+
+
+
+
+  constructor() {
+
+    //Start create a canvas element ++++++++++++++++++++++++++++++++
+    this.canvas = document.createElement("canvas");
+    this.canvas.style.backgroundColor = 'white';
+    //check if canvas was made
+    if(!this.canvas || !this.canvas.getContext)
+    throw "No valid canvas found!";
+    //match the dimensions of the canvas div
+    this.canvas.width = 1000;
+    this.canvas.height = 750;
+    //make the canvas a child of the canvas div
+    document.getElementById('canDiv').appendChild(this.canvas);
+
+    //  create the context for the canvas
+    this.context = this.canvas.getContext("2d");
+    //check if context was made
+    if(!this.context)
+    throw "No valid context found!";
+    //End create a canvas element ++++++++++++++++++++++++++++++++
+    // declare instance variables for main
+    this.menuButtons = [];
+    this.makeRect = false;
+    this.grav = false;
+    //this.b = new Boid(this, vector2d(300, 300));
+    //create all initial items
+    this.init();
+    this.boids = [];
+    this.fillarr();
+
+    r = new Repeller(this, vector2d(Math.random()*1000, Math.random()*750))
+
+    this.gravity = false;
+  }
+
+  fillarr(){
+    for(var i = 0; i < NUM_BOIDS; i++ ){
+      this.boids.push(new Boid(this, vector2d(Math.random()*1000, Math.random()*750)));
+    }
+  }
+  init(){
+    // get the current time
+    this.lastTime = Date.now();
+    // select canvas for callbacks
+    this.canvas.addEventListener('mousemove',this.handleCNVMouseMoved,false);
+    this.canvas.addEventListener('mouseover',this.handleCNVMouseOver, false);
+    this.canvas.addEventListener('click', this.handleCNVMouseClicked, false);
+
+    // create menu buttons
+    this.createMenuButtons();
+
+  }
+
+  run() { // update canvas components --> called from draw()
+
+    this.render();
+        this.context.rect(0,0, 1000, 750);
+    this.context.fillStyle = 'black';
+    this.context.fill();
+    this.context.beginPath();
+
+    this.context.stroke();
+    for(var i = 0; i < NUM_BOIDS; i++ ){
+      this.boids[i].run();
+
+    }
+    r.run()
+
+
+  }
+
+  render() { // render or draw stuff to canvas
+    //this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+    if(this.makeRect){
+      this.context.fillStyle = '#554499';
+      this.context.fillRect(10, 10, 100, 100);
+    }
+
+  }
+  //  +++++++++++++++++++++++++++++++++  create buttons for menu area
+  createMenuButtons(){
+
+     var numButtons = 5;
+     //create and style all button divs
+     for(let i = 0; i < numButtons; i++){
+       // create a button and place it on the DOM tree
+       var button = document.createElement('div');
+       document.getElementById("menuDiv").appendChild(button);
+       // place a button image on the button
+       var buttImg = new Image();
+       buttImg.src = "buttons/mb01.png";
+       buttImg.id = "bi";
+       button.appendChild(buttImg);
+       //  Add event listeners to images (not buttons)
+       buttImg.addEventListener('mouseover',buttonMouseOverHandler,false);
+       buttImg.addEventListener('mouseout',buttonMouseOutHandler,false);
+       buttImg.addEventListener('click',buttonMouseClickHandler,false);
+       // style buttons
+       button.style.float = "left";
+       button.style.marginTop = "5px";
+       button.style.marginLeft = "85px";
+
+       //push button into buttons array
+       this.menuButtons.push(button);
+     }
+
+  }   // end createMenuButtons
+   gravity(){
+     if(main.grav){
+     boid.speedY = -9.8;
+     boid.speedX = 0;
+   }
+   }
+
+   handleCNVMouseClicked(){
+     this.mouseX = event.clientX;
+     this.mouseY = event.clientY;
+console.log(event)
+   }
+
+
+}//  end main class ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// add functionality to your buttons here
+
+function mouse_position()
+{
+    var e = window.event;
+
+    var mousPosX = e.clientX;
+    var mousPosY = e.clientY;
+
+    document.Form1.posx.value = posX;
+    document.Form1.posy.value = posY;
+
+    var t = setTimeout(mouse_position,100);
+
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function buttonMouseOverHandler(){
+   this.src = "buttons/mb02.png"
+}
+
+
+
+
+function buttonMouseOutHandler(){
+  this.src = "buttons/mb01.png"
+}
+
+function buttonMouseClickHandler(){
+  main.makeRect = !main.makeRect;
+  main.gravity = !main.gravity;
+}
